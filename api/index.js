54a7a959cb5bbed7e3a2d247146d81d762ea6066
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-04-30 17:51:09 +0500

    Add routes for the App.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv"
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express()
 dotenv.config();
@@ -16,12 +20,12 @@ const connect = async () => {
 
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!")
-})
+});
 
-mongoose.connection.on("connected", () => {
-  console.log("mongoDB connected!")
-})
-
+app.use("/api/auth", authRoute);
+app.use("/api/users", authRoute);
+app.use("/api/hotels", authRoute);
+app.use("/api/rooms", authRoute);
 
 app.listen(8800, () => {
   connect()
