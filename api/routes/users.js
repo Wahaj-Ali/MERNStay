a5bb1d8b531d018e394931a5dec2907bae44eb66
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-06-03 23:01:06 +0500

    Update users.js
    Add routes for user authentication

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,17 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user_controller.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("hello user, you are logged in");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+    res.send("hello user, you are logged in and can delete your account");
+});
 
 //UPDATE
 router.put("/:id", updateUser);
