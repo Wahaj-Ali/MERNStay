fa85e8ec03dfcb570942c3a3d2e7ae34387fc090
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-07-17 21:49:56 +0500

    Update room_controller.js

--- api/controllers/room_controller.js
+++ api/controllers/room_controller.js
@@ -1,6 +1,6 @@
-import Hotel from "../models/Hotel";
-import Room from "../models/Room";
-import { createError } from "../utils/error";
+import Hotel from "../models/Hotel.js";
+import Room from "../models/Room.js";
+import { createError } from "../utils/error.js";
 
 export const createRoom = async (req, res, next) => {
     const hotelId = req.params.hotelid;
@@ -23,7 +23,7 @@ export const createRoom = async (req, res, next) => {
 
 export const updateRoom = async (req, res, next) => {
     try {
-      const updatedHotel = await Room.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+      const updateRoom = await Room.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
       res.status(200).json(updateRoom);
     } catch (err) {
       next(err);
