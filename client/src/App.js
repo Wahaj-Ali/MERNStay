09602a09d6e55e34feeccbc45314cf5fb70cfd37
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-07-21 02:55:50 +0500

    Update App.js
    Add pages in the app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
+import { Home } from './pages/Home';
+import { List } from './pages/List';
+import { SingleHotel } from './pages/singleHotel';
+
+
 function App() {
   return (
     <div className="App">
-        <h1>MERN Stay</h1>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/hotels' element={<List />}/>
+            <Route path='/hotel' element={<SingleHotel />}/>
+          </Routes>
+        </Router>
     </div>
   );
 }
