673e6d76f2def99655df5733fdcec24b3ca6938e
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-07-28 02:06:10 +0500

    Update MUI version.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar/Navbar'
+import { Header } from '../components/Header/Header'
 
 export const Home = () => {
   return (
     <div>
         <Navbar />
+        <Header />
     </div>
   )
 }
