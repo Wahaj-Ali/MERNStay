45a673b50a873b80d7bce9c71c087c43154e4cae
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-07-28 02:52:55 +0500

    Update Header.jsx

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -30,7 +30,7 @@ export const Header = () => {
         <img id='hero-img' src={herobg} alt='hero-background'/>
         <div className="hero-desc">
           <h1 className="headerTitle">Homebound Wanderlust: Embrace the Journey.</h1>
-          <Typography variant="body1" sx={{ display: 'flex', margin: '20px 0' }}>Where Joyful Homecomings Meet Thrilling Travels. Discover, Book, and Embrace Unforgettable Adventures.</Typography>
+          <Typography variant="h6" sx={{ display: 'flex', margin: '20px 0', fontWeight:600, fontFamily:'"Avenir Next",BlinkMacSystemFont,-apple-system,Segoe UI,Roboto,Helvetica,Arial,sans-serif;' }}>Where Joyful Homecomings Meet Thrilling Travels. Discover, Book, and Embrace Unforgettable Adventures.</Typography>
         </div>
       </div>
     </div>
