99df038d4220bab38f841bfab34aeabd60176780
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-08-02 18:30:29 +0500

    Update Home.jsx
    Add Trending component to the home page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Navbar } from '../components/Navbar/Navbar'
-import { Header } from '../components/Header/Header'
+import { Navbar } from '../components/Navbar/Navbar';
+import { Header } from '../components/Header/Header';
+import { Trending } from '../components/trending/Trending';
+import '../assets/styles/Home.css';
 
 export const Home = () => {
   return (
     <div>
         <Navbar />
         <Header />
+        <div className="homeContainer">
+          <Trending />
+        </div>
     </div>
   )
 }
