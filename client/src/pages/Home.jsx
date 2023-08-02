f2508ab48764eb8f62400aaab2430e19e2064b19
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-08-02 21:35:56 +0500

    Update Home.jsx
    Add "Unique Properties" component to the home page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,17 +4,23 @@ import { Header } from '../components/Header/Header';
 import { Trending } from '../components/trending/Trending';
 import '../assets/styles/Home.css';
 import { PropertyTypes } from '../components/PropertyTypes/PropertyTypes';
+import { UniqueProperties } from '../components/UniqueProperties/UniqueProperties';
 
 export const Home = () => {
   return (
     <div>
-        <Navbar />
-        <Header />
-        <div className="homeContainer">
-          <Trending />
-          <h1 className="typeHeading">Browse by property type</h1>
-          <PropertyTypes />
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Trending />
+        <h1 className="homeHeading">Browse by property type</h1>
+        <PropertyTypes />
+        <div className="uniquePDesc">
+          <h1 className="homeHeading">Stay at our top unique properties</h1>
+          <h2 className="homeText">From castles and villas to boats and igloos, we have it all</h2>
         </div>
+        <UniqueProperties />
+      </div>
     </div>
   )
 }
