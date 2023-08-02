b9f4cd32a4704cbbf0908920c93dbce8c22533b3
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-08-02 19:01:27 +0500

    Update Trending.jsx
    Design the trending component.

--- client/src/components/trending/Trending.jsx
+++ client/src/components/trending/Trending.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import trnd1 from '../../assets/trending1.jpg';
+import trnd2 from '../../assets/trending2.jpg';
 import './Trending.css';
 
 export const Trending = () => {
   return (
-    <div>Trending</div>
+    <div className='trending'>
+      <div className="trendingItem">
+        <img src={trnd2} alt="trending" className='trendingImg'/>
+        <div className="trendingTitles">
+          <h1>Dublin</h1>
+          <h2>100 properties</h2>
+        </div>
+      </div>
+      <div className="trendingItem">
+        <img src={trnd1} alt="trending" className='trendingImg'/>
+        <div className="trendingTitles">
+          <h1>Moscow</h1>
+          <h2>120 properties</h2>
+        </div>
+      </div>
+      <div className="trendingItem">
+        <img src={trnd2} alt="trending" className='trendingImg'/>
+        <div className="trendingTitles">
+          <h1>Moscow</h1>
+          <h2>120 properties</h2>
+        </div>
+      </div>
+    </div>
   )
 }
