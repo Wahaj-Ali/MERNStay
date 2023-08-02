841ad49504d622b9e519560a4f429bcc7d9cab3e
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-08-02 18:12:27 +0500

    Update List.js
    Add navbar and customized header component to the hotels page.

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Navbar } from '../components/Navbar/Navbar'
+import { Header } from '../components/Header/Header'
 
 export const List = () => {
   return (
-    <div>Hotels</div>
+    <div>
+      <Navbar />
+      <Header  type="list"/>
+    </div>
   )
 }
