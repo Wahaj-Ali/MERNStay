0fa0496055b9c3b8118da287e30c8dfc667a5db3
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-08-02 20:26:22 +0500

    Update Home.jsx
    Import "Property Types" component in the home page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Navbar } from '../components/Navbar/Navbar';
 import { Header } from '../components/Header/Header';
 import { Trending } from '../components/trending/Trending';
 import '../assets/styles/Home.css';
+import { PropertyTypes } from '../components/PropertyTypes/PropertyTypes';
 
 export const Home = () => {
   return (
@@ -11,6 +12,8 @@ export const Home = () => {
         <Header />
         <div className="homeContainer">
           <Trending />
+          <h1 className="typeHeading">Browse by property type</h1>
+          <PropertyTypes />
         </div>
     </div>
   )
