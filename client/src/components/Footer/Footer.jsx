5161932c5fe2cdf7d279ff886a0850636c0ff240
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-08-04 23:01:08 +0500

    Create Footer.jsx
    Add a footer component.

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import './Footer.css';
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className="fLists">
+        <ul className="fList">
+          <li className="fLIstItem">Countires</li>
+          <li className="fLIstItem">Regions</li>
+          <li className="fLIstItem">Cities</li>
+          <li className="fLIstItem">Districts</li>
+          <li className="fLIstItem">Airports</li>
+          <li className="fLIstItem">Hotels</li>
+          <li className="fLIstItem">Places of interest</li>
+        </ul>
+        <ul className="fList">
+          <li className="fLIstItem">Homes</li>
+          <li className="fLIstItem">Apartments</li>
+          <li className="fLIstItem">Resorts</li>
+          <li className="fLIstItem">Villas</li>
+          <li className="fLIstItem">Hostels</li>
+          <li className="fLIstItem">B&Bs</li>
+          <li className="fLIstItem">Guest houses</li>
+        </ul>
+        <ul className="fList">
+          <li className="fLIstItem">Unique places to stay</li>
+          <li className="fLIstItem">All destinations</li>
+          <li className="fLIstItem">All flight destinations</li>
+          <li className="fLIstItem">All car rental locations</li>
+          <li className="fLIstItem">Discover</li>
+          <li className="fLIstItem">Reviews</li>
+          <li className="fLIstItem">Discover monthly stays</li>
+          <li className="fLIstItem">Unpacked: Travel articles</li>
+          <li className="fLIstItem">Seasonal and holiday deals</li>
+          <li className="fLIstItem">Traveller Review Awards</li>
+        </ul>
+        <ul className="fList">
+          <li className="fLIstItem">Car rental</li>
+          <li className="fLIstItem">Flight finder</li>
+          <li className="fLIstItem">Restaurant reservations</li>
+          <li className="fLIstItem">Booking.com for Travel Agents</li>
+        </ul>
+        <ul className="fList">
+        <li className="fLIstItem">Coronavirus (COVID-19) FAQs</li>
+        <li className="fLIstItem">About Booking.com</li>
+        <li className="fLIstItem">Customer Service Help</li>
+        <li className="fLIstItem">Partner help</li>
+        <li className="fLIstItem">Careers</li>
+        <li className="fLIstItem">Sustainability</li>
+        <li className="fLIstItem">Press Center</li>
+        <li className="fLIstItem">Safety Resource Center</li>
+        <li className="fLIstItem">Investor relations</li>
+        <li className="fLIstItem">Terms & Conditions</li>
+        <li className="fLIstItem">Partner dispute</li>
+        <li className="fLIstItem">How We Work</li>
+        <li className="fLIstItem">Privacy & cookie statement</li>
+        <li className="fLIstItem">MSA statement</li>
+        <li className="fLIstItem">Corporate contact</li>
+        <li className="fLIstItem">Content guidelines</li>
+        </ul>
+      </div>
+      <p>Copyright &copy; Wahaj 2023</p>
+    </div>
+  )
+}
